076f6add2af19d713290bc470eaf54408af0ba88
Sirivat Asanasuwan <sirivatasa@outlook.de>
2023-10-13 16:57:48 +0200

    update lambda trigger

---
+++ modules/aws-eventbridge-weather-lambda-trigger/variables.tf
@@ -0,0 +1,9 @@
+variable "get_real_time_weather_lambda_arn" {
+    description = "The ARN of the getRealTimeWeather Lambda function"
+    type        = string
+}
+
+variable "get_weather_forecast_lambda_arn" {
+    description = "The ARN of the getWeatherForecast Lambda function"
+    type        = string
+}
