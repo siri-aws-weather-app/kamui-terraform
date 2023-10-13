6333334c484663180144ab45224bcf40341ed92b
Sirivat Asanasuwan <sirivatasa@outlook.de>
2023-10-13 16:57:48 +0200

    update lambda trigger

---
+++ accounts/development/eu-central-1/development/infrastructure/aws-eventbridge/weather-lambda-trigger/terragrunt.hcl
@@ -0,0 +1,20 @@
+# Terragrunt configuration
+
+include "root" {
+  path = find_in_parent_folders()
+}
+
+locals {
+  # Expose the base source path
+  base_source = "${dirname(find_in_parent_folders())}/../modules/aws-eventbridge-weather-lambda-trigger"
+}
+
+# Set the location of Terraform configurations
+terraform {
+  source = local.base_source
+}
+
+inputs = {
+  get_real_time_weather_lambda_arn = "arn:aws:lambda:eu-central-1:360532638835:function:getRealTimeWeather"
+  get_weather_forecast_lambda_arn = "arn:aws:lambda:eu-central-1:360532638835:function:getWeatherForecast"
+}
---
+++ modules/aws-eventbridge-weather-lambda-trigger/main.tf
@@ -0,0 +1,25 @@
+module "eventbridge" {
+  source = "terraform-aws-modules/eventbridge/aws"
+
+  bus_name = "lambda-weather-trigger"
+
+  attach_lambda_policy = true
+  lambda_target_arns   = [var.get_real_time_weather_lambda_arn, var.get_weather_forecast_lambda_arn]
+
+  schedules = {
+    get-real-time-weather = {
+      description         = "Trigger for getRealTimeWeather lambda function."
+      schedule_expression = "rate(60 minutes)"
+      timezone            = "Europe/Berlin"
+      arn                 = var.get_real_time_weather_lambda_arn
+      input               = jsonencode({ "job" : "cron-by-rate" })
+    }
+    get-weather-forecast = {
+      description         = "Trigger for getWeatherForecast lambda function."
+      schedule_expression = "rate(1 day)"
+      timezone            = "Europe/Berlin"
+      arn                 = var.get_weather_forecast_lambda_arn
+      input               = jsonencode({ "job" : "cron-by-rate" })
+    }
+  }
+}
